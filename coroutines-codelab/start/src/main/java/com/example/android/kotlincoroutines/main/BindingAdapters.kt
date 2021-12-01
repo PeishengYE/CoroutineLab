bf164624e958481ccaf46fdd83c3fcdd3fc6e352
Peisheng Ye <pye@securustechnologies.com>
2021-12-01 16:17:10 -0500

    fixed build gradle sync error

---
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/BindingAdapters.kt
@@ -0,0 +1,20 @@
+package com.example.android.kotlincoroutines.main
+
+import android.widget.ImageView
+import com.example.android.kotlincoroutines.R
+import androidx.databinding.BindingAdapter
+/**
+ * Uses the Glide library to load an image by URL into an [ImageView]
+ */
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+                .load(imgUri)
+                .apply(RequestOptions()
+                        .placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image))
+                .into(imgView)
+    }
+}
