9c509bab527b07d83c9a32411eae1dc8ec80ac8f
Peisheng Ye <pye@securustechnologies.com>
2021-12-01 16:41:06 -0500

    fixed error on compiling dependency

--- coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/BindingAdapters.kt
+++ coroutines-codelab/start/src/main/java/com/example/android/kotlincoroutines/main/BindingAdapters.kt
@@ -3,6 +3,10 @@ package com.example.android.kotlincoroutines.main
 import android.widget.ImageView
 import com.example.android.kotlincoroutines.R
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import androidx.core.net.toUri
+import com.bumptech.glide.request.RequestOptions
+
 /**
  * Uses the Glide library to load an image by URL into an [ImageView]
  */
